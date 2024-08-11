e3f50f20273d058f70a4e327e7a5e4ed38e7c9cd
Ann Berinceva <berinceva92@bk.ru>
2024-08-11 20:13:17 +0500

    first commit

---
+++ src/main/java/Feline.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class Feline extends Animal implements Predator {
+
+    @Override
+    public List<String> eatMeat() throws Exception {
+        return getFood("Хищник");
+    }
+
+    @Override
+    public String getFamily() {
+        return "Кошачьи";
+    }
+
+    public int getKittens() {
+        return getKittens(1);
+    }
+
+    public int getKittens(int kittensCount) {
+        if (kittensCount <= 0) {
+            return 1; // Если число котят 0 или меньше, то вернуть 1
+        } else {
+            return kittensCount;
+        }
+    }
+
+}
---
+++ src/main/java/Lion.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class Lion {
+
+    private Feline feline;
+    private boolean hasMane;
+
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
+        if ("Самец".equals(sex)) {
+            hasMane = true;
+        } else if ("Самка".equals(sex)) {
+            hasMane = false;
+        } else {
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+        }
+    }
+
+    //Feline feline = new Feline();
+
+    public int getKittens() {
+
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
+    public List<String> getFood() throws Exception {
+        return feline.eatMeat();
+    }
+}
